0846a1c901c226a519e5fb369d8732c5a09cad4e
Tisax190 <Tisax1900@hotmail.fr>
2019-04-30 15:45:00 +0200

    Register passager

---
+++ Covoiturage/Covoiturage/Models/DAL/DALPassager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Covoiturage.Models.POCO;
+
+namespace Covoiturage.Models.DAL
+{
+    public class DALPassager
+    {
+        private BddContext bdd;
+        public DALPassager()
+        {
+            bdd = new BddContext();
+        }
+
+        public void RegisterPassager(Passager passager)
+        {
+            bdd.ListePassager.Add(passager);
+        }
+    }
+}
