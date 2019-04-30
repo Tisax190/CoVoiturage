cabb11e9fb6f33de5d181974697f87b0a4adc328
Axel BOL <axel.bol@condorcet.be>
2019-04-30 15:52:57 +0200

    Add "Ville" and "Voiture"

---
+++ Covoiturage/Covoiturage/Models/POCO/Voiture.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Covoiturage.Models.POCO
+{
+    public class Voiture
+    {
+        public string Plaque { get; set; }
+        public string Modele { get; set; }
+        public int PlaceDisponible { get; set; }
+
+        public void ChangePlaque()
+        {
+
+        }
+    }
+}
